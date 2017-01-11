f47b4078173fe4d59415d0942a96f6b8c22d4ced
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-01-11 10:48:38 +0100

    lezione and or

---
+++ 03-and-or.py
@@ -0,0 +1,17 @@
+print('programma protetto da password')
+username = input('username: ')
+password = input('password: ')
+
+if username == 'tizio' and password == 'caio':
+  print('accesso effettuato')
+  anni = int(input('quanti anni hai? '))
+  if anni < 18 or anni > 80:
+    print('sei o troppo giovane o troppo anziano')  
+  else:
+    print('i tuoi anni sono compresi tra 18 e 80')
+	
+else:  
+  print('accesso negato!')
+
+  
+print('ciao ciao');
