93b12e4e38ad6e247102210ed63f08b828b96489
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-02-01 09:06:52 +0100

    mods

--- 05-fortunello.py
+++ 05-fortunello.py
@@ -1,18 +1,6 @@
-print('programma protetto da password')
-username = input('username: ')
-password = input('password: ')
-  
-if username == 'tizio' and password == 'caio':
-  print('accesso effettuato')
-  anni = input('quanti anni hai?')
-
-  if anni < 18 or anni > 80:
-    print('sei o troppo giovane o troppo anziano')  
-  else:
-    print('i tuoi anni sono compresi tra 18 e 80')
-	
-else:  
-  print('accesso negato!')
-
-  
-print('ciao ciao');
+from random import randint
+print("Benvenuto in programma fortunello!")
+numero = int(input("Tra quante persone devo estrarre? "))
+print("Il fortunello di oggi è............")
+input()
+print(randint(1, numero))
