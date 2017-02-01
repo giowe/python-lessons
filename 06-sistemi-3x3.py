a392f80b53febe5ee13373993713f838255cdb29
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-02-01 09:55:08 +0100

    aggiunto compti a casa

---
+++ 06-sistemi-3x3.py
@@ -0,0 +1,31 @@
+# Risolvere un sistema di 3 equazioni in 3 incognite applicando il metodo di Cramer
+
+print('ax + by + c = 0')
+a = float(input('inserisci "a"' ))
+b = float(input('inserisci "b"' ))
+c = float(input('inserisci "c"' ))
+
+print('a1x + b1y + c1 = 0')
+a1 = float(input('inserisci "a1"' ))
+b1 = float(input('inserisci "b1"' ))
+c1 = float(input('inserisci "c1"' ))
+
+print('a2x + b2y + c2 = 0')
+a2 = float(input('inserisci "a2"' ))
+b2 = float(input('inserisci "b2"' ))
+c2 = float(input('inserisci "c2"' ))
+
+#calcolare il determinante della matrice D
+
+#se D = 0 il sistema e'indeterminato ed esce
+
+#calcolo il determinante della matrice delle X   (DX)
+
+#calcolo il determinante della matrice delle Y   (DY)
+
+#calcolo il determinante della matrice delle Z   (DZ)
+
+#calcolo il valore della X Y Z
+
+#stampo i risultati
+
