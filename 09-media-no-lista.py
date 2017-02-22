980d680c0807dcdd34db8dafad4f02027002bf75
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-02-22 10:44:17 +0100

    add

---
+++ 09-media-no-lista.py
@@ -0,0 +1,15 @@
+materia = input('Dimmi di che materia stiamo parlando? ')
+sommatoria = 0
+nVoti = 0
+while True:
+	votoCorrente = float(input('inserisci un voto: '))
+	if votoCorrente > 10 or votoCorrente < 0:
+		print('hai inserito', votoCorrente,', non è un valore accettabile.')
+	elif votoCorrente == 0:
+		break
+	else:
+		nVoti += 1
+		sommatoria += votoCorrente
+
+media = sommatoria / nVoti
+print('Fino ad ora in', materia, 'hai preso', nVoti, 'voti la cui media è', media)			
