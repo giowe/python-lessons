b83925ba3f434c205e786a8c93211e7f0859d1d1
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-04-19 11:51:43 +0200

    aggiunto programma spesa

---
+++ 15-spesa.py
@@ -0,0 +1,43 @@
+carrello = []
+negozio = [
+  'pane',
+  'burro',
+  'salame',
+  'prodotto costosisssssimo',
+  'lattuga',
+  'pomodoro'
+]
+
+def acquista(indiceProdotto):
+  nomeProdotto = negozio[indiceProdotto]
+  carrello.append(nomeProdotto)
+  return nomeProdotto
+
+def prezzo(nomeProdotto):
+  return len(nomeProdotto) * 0.1
+
+def checkout():
+  prezzoTotale = 0
+  for prodotto in carrello:
+    prezzoTotale += prezzo(prodotto)
+  return prezzoTotale
+
+print('Benvenuto nel mio modesto negozietto, quele vorresti comperare tra i prodotti che ho da offrire?') 
+for i in range(len(negozio)):
+  print(i+1, ')', negozio[i])
+
+while True:
+  indiceProdotto = int(input('che prodotto vuoi comperare? '))
+  if indiceProdotto == 666:
+    break
+  else:
+    prodottoAcquistato = acquista(indiceProdotto-1)    
+    print('hai comperato', prodottoAcquistato, 'al modico prezzo di', prezzo(prodottoAcquistato), 'euro')
+    print('al momento hai speso', checkout(), 'euro')
+    print()
+
+print('grazie per aver fatto la spesa da noi! <3')    
+print('puoi pagare in cassa', checkout(), 'euro')
+print('hai acquistato:')
+for prodotto in carrello:
+  print(prodotto)
