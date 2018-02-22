e9d23c3f3572f481efbfdf750022d11a2e61a163
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-02-22 08:34:27 +0100

    added input output

---
+++ 18-fileIn-fileOut.py
@@ -0,0 +1,25 @@
+def readFile(fileName): 
+  out = []
+  file = open(fileName)
+  r = file.readlines()
+  file.close()
+
+  for e in r:
+    out.append(e.replace("\n", ""))
+
+  return out
+
+def saveFile(fileName, list):
+  file = open(fileName, "w")
+  for r in list:
+    file.write(r + "\n")
+  file.close()
+
+carrello = readFile("./18-input-file.txt")
+
+for e in carrello:
+  print(e)
+
+print("aggiungo cioccolata al carrello e salvo il file")
+carrello.append("cioccolata")
+saveFile("./18-output-file.txt", carrello)
