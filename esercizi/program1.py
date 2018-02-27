6d98ebcc6042d3d84deb885d04da1ed41c27f12c
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-02-27 11:51:54 +0100

    mods

--- esercizi/program1.py
+++ esercizi/program1.py
@@ -1,13 +1,14 @@
 '''
+***********************PROGRAM_1***********************
 CONSEGNA:
 data una lista di numeri, sommali e salva l'output
 
-INPUT EX (./input1.txt):
+***********************INPUT EX (./input1.txt):
 1
 3
 4
 
-OUTPUT EX (./output1.txt):
+***********************OUTPUT EX (./output1.txt):
 8
 
 '''
