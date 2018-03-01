6c4c4adfb3a77b18724ddc3e0e4a20523027103c
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-03-01 08:07:44 +0100

    program2

---
+++ esercizi/program2.py
@@ -0,0 +1,16 @@
+'''
+***********************PROGRAM_2***********************
+CONSEGNA:
+data una lista di prodotti elimina la cioccolata
+e salva la lista aggiornata
+
+***********************INPUT EX (./input2.txt):
+mele
+cioccolata
+banane
+
+***********************OUTPUT EX (./output2.txt):
+mele
+banane
+
+'''
