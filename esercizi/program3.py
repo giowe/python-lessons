2bbc7e7f1c5930fa5e2491419b5a807ea598b4d6
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-03-01 08:15:17 +0100

    program 4 & 5

---
+++ esercizi/program3.py
@@ -0,0 +1,17 @@
+'''
+***********************PROGRAM_3***********************
+CONSEGNA:
+data una lista di numeri moltiplicali per due e salva
+la lista aggioranta
+
+***********************INPUT EX (./input3.txt):
+2
+3
+5
+
+***********************OUTPUT EX (./output3.txt):
+4
+6
+10
+
+'''
---
+++ esercizi/program4.py
@@ -0,0 +1,19 @@
+'''
+***********************PROGRAM_3***********************
+CONSEGNA:
+data una lista di numeri moltiplicali per il numero
+che li precede e salva la lista aggioranta.
+Il primo numero della lista deve essere moltiplicato
+per 1.
+
+***********************INPUT EX (./input3.txt):
+2
+3
+5
+
+***********************OUTPUT EX (./output3.txt):
+2
+6
+15
+
+'''
